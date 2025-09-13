8c07b5a74c43f44cf2fe0bdb9aa66759e31d2037
waltinho0807 <waltinhocalegari@hotmail.com>
2025-09-12 22:01:39 -0300

    first commit

--- app/api/check-membership/route.ts
+++ app/api/check-membership/route.ts
@@ -1,1 +1,54 @@
 import { NextResponse } from 'next/server';
+
+export async function POST (req: Response) {
+    const botToken = process.env.BOT_TOKEN;
+
+    if(!botToken) {
+        return NextResponse.json({ error: "Telegran bot is missing" }, { status: 500 });
+    }
+
+    const { telegranId, channelUsername } = await req.json();
+
+    if(!telegranId || channelUsername) {
+        return NextResponse.json({ error: 'Invalid request: missing telegranId or channelUsername' }, { status: 400 });
+    }
+
+    try {
+        let formattedChatId = channelUsername;
+        if(!channelUsername.startsWith('@') && !channelUsername.startsWith('-100')){
+            formattedChatId = '@' + channelUsername;
+        }
+
+        const url = `https://api.telegran.org/bot${botToken}/getChatMember?chat_id=${encodeURIComponent(formattedChatId)}&user_id=${telegranId}`;
+
+        const response = await fetch(url);
+
+        if(!response.ok) {
+            const errorText = await response.text();
+            console.error('Telegram API error: ', response.status, errorText);
+            return NextResponse.json({error: `Telegram API error: ${response.status} ${errorText}`}, { status: 500 })
+        } 
+        
+        const data = await response.json();
+
+        if(data.ok) {
+
+           const status = data.result.status;
+           const isMember = ['Creator', 'Administrator', 'Member'].includes(status);
+           return NextResponse.json({isMember});
+
+        } else {
+            return NextResponse.json({error: `Telegram API returned false: ${JSON.stringify(data)}`})
+        }
+        
+    } catch (error) {
+        console.error('Error checking channel membership', error);
+        if (error instanceof Error) {
+            return NextResponse.json({error: `Failed to check channel membership: ${error.message}`}, { status: 500 });
+        }
+
+        return NextResponse.json({error: 'An unknow error ocurred while checking channel membership'}, { status: 500 });
+    }
+
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,116 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import { setRequestMeta } from "next/dist/server/request-meta";
+import { useEffect, useState } from "react";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+declare global{
+  interface Window {
+    Telegram?: {
+      WebApp?: any
+    }
+  }
 }
+
+export default function Home () {
+  const [isChannelMember, setChannelMember] = useState<Boolean | null> (null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [TelegramId, setTelegramId] = useState<String| null>(null);
+  const [channelUsername, setChannelUsername] = useState('');
+  const [error, setError] = useState<Boolean| null>(null);
+
+  useEffect(() => {
+    if(window.Telegram?.WebApp) {
+      const initDataString = window.Telegram.WebApp.initData;
+      if(initDataString) {
+        const urlParams = new URLSearchParams(initDataString);
+        try {
+          const user = JSON.parse(urlParams.get('user') || '{}');
+          if(user.id){
+            setTelegramId(user.id.toString())
+          }
+        }catch (error) {
+          console.error('Error parsing data', error);
+        }
+      }
+    }
+  }, []);
+
+  const checkChannelMembership = async () => {
+    if(!TelegramId) {
+      alert('This app can only be use Telegram ');
+      return
+    }
+
+    if(!channelUsername) {
+      alert('Please enter username');
+      return
+    }
+
+    setIsLoading(true);
+
+  try {
+    const response = await fetch('/api/check-membership', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        TelegramId,
+        channelUsername
+      }) 
+    })
+     
+    if(!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error || 'Failed to check membership')
+    }
+
+    const data = await response.json()
+    setChannelMember(data.isMember);
+    setError(null);
+    
+  } catch (error) {
+    console.error('Error checking channel membership', error);
+    setChannelMember(false);
+    setError(error instanceof Error)
+  }finally {
+    setIsLoading(false)
+  }
+
+  }
+
+  if(!TelegramId) {
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <h1 className="text-4x1 font-bold mb-8">Telegram membership check</h1>
+      <p className="text-xl">This app can only be used whitchin telegram</p>
+    </main>
+  }
+
+ 
+
+  return(
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <h1 className="text-4x1 font-bold mb-8">Telegram channel membership check</h1>
+      <input
+        type="text"
+        value={channelUsername}
+        onChange={(e) => setChannelUsername(e.target.value)}
+        placeholder="Enter channel username @example"
+        className="mb-4 p-2 border border-gray-300 rounded w-full max-w-xs"
+      />
+      <button
+        onClick={checkChannelMembership}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        disabled={isLoading || !channelUsername}
+      >
+        {isLoading ? 'Checking... ': 'Check membership'}
+      </button>
+      {error && <p className="mt-4 text-red-500">{false}</p>}
+      {isChannelMember !== null && !isLoading && (
+        <p className="mt-4 text-xl">
+          {isChannelMember? 'You are member of the channel' : 'You are not member the channel'}
+        </p>
+      )}
+    </main>
+  )
+} 
