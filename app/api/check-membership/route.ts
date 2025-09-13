2d0daf89a3829607b95568f68ec26d55cb830787
waltinho0807 <waltinhocalegari@hotmail.com>
2025-09-12 22:10:27 -0300

    werter

--- app/api/check-membership/route.ts
+++ app/api/check-membership/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST (req: NextResponse) {
+export async function POST (req: NextRequest) {
     const botToken = process.env.BOT_TOKEN;
 
     if(!botToken) {
